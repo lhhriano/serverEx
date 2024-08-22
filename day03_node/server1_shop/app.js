d782ceaabd62098d12cda368a13adac88ed48892
rianolhh <lhh964513@gmail.com>
2024-08-22 11:09:53 +0900

    1

--- day03_node/server1_shop/app.js
+++ day03_node/server1_shop/app.js
@@ -9,36 +9,61 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/home", (req, res) => {
-    req.app.render("home/Home", {}, (err, html)=>{
-        res.end(html);
+    req.app.render("home/Home", {}, (err, html) => {
+        if (err) {
+            console.error("Error rendering home:", err);
+            res.status(500).send("Internal Server Error");
+            return;
+        }
+        res.send(html);
     });
 });
 
 app.get("/profile", (req, res) => {
-    req.app.render("profile/Profile", {}, (err, html)=>{
-        res.end(html);
+    req.app.render("profile/Profile", {}, (err, html) => {
+        if (err) {
+            console.error("Error rendering profile:", err);
+            res.status(500).send("Internal Server Error");
+            return;
+        }
+        res.send(html);
     });
 });
 
 app.get("/member", (req, res) => {
-    req.app.render("member/Member", {}, (err, html)=>{
-        res.end(html);
+    req.app.render("member/Member", {}, (err, html) => {
+        if (err) {
+            console.error("Error rendering member:", err);
+            res.status(500).send("Internal Server Error");
+            return;
+        }
+        res.send(html);
     });
 });
 
 app.get("/gallery", (req, res) => {
-    req.app.render("gallery/Gallery", {}, (err, html)=>{
-        res.end(html);
+    req.app.render("gallery/Gallery", {}, (err, html) => {
+        if (err) {
+            console.error("Error rendering gallery:", err);
+            res.status(500).send("Internal Server Error");
+            return;
+        }
+        res.send(html);
     });
 });
 
 app.get("/shop", (req, res) => {
-    req.app.render("shop/Shop", {}, (err, html)=>{
-        res.end(html);
+    req.app.render("shop/Shop", {}, (err, html) => {
+        if (err) {
+            console.error("Error rendering shop:", err);
+            res.status(500).send("Internal Server Error");
+            return;
+        }
+        res.send(html);
     });
 });
 
 const server = http.createServer(app);
-server.listen(app.get('port'), ()=>{
+server.listen(app.get('port'), () => {
     console.log(`Run on server >>> http://localhost:${app.get('port')}`);
 });
