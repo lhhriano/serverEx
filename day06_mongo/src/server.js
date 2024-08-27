c30a623b7032d6a8ab9f4e9a338bd386556c37f8
rianolhh <lhh964513@gmail.com>
2024-08-27 09:38:16 +0900

    1

--- day06_mongo/src/server.js
+++ day06_mongo/src/server.js
@@ -19,55 +19,55 @@ const dbName = "vehicle";
 const collectionName = "car";
 
 app.get("/car", async (req, res) => {
-  try {
-    client.connect();
-    const database = client.db(dbName);
-    const cars = database.collection(collectionName);
-    const query = {};
-    const options = {
-      sort: { name: 1 },
-      //projection: { _id:true, name:1, company:1, price:1, year:1 }
-      projection: {}
-    };
-    const cursor = cars.find(query, options);
-    if ((await cars.countDocuments(query)) === 0) {
-      console.log("No documents found!");
+    try {
+      client.connect();
+      const database = client.db(dbName);
+      const cars = database.collection(collectionName);
+      const query = {};
+      const options = {
+        sort: { name: 1 },
+        //projection: { _id:true, name:1, company:1, price:1, year:1 }
+        projection: {}
+      };
+      const cursor = cars.find(query, options);
+      if ((await cars.countDocuments(query)) === 0) {
+        console.log("No documents found!");
+      }
+      const carList = [];
+      for await (const doc of cursor) {
+        carList.push(doc);
+        console.dir(doc);
+      }
+      req.app.render("CarList", {carList}, (err, html)=>{
+        res.end(html);
+      });
+    } finally {
+      await client.close();
     }
-    const carList = [];
-    for await (const doc of cursor) {
-      carList.push(doc);
-      console.dir(doc);
-    }
-    req.app.render("CarList", {carList}, (err, html)=>{
-      res.end(html);
+  });
+  
+
+    app.post("/car", async (req, res)=> {
+        try {
+        client.connect();
+        const database = client.db(dbName);
+        const cars = database.collection(collectionName);
+        const doc = {
+            name: req.body.name,
+            price: req.body.price,
+            company: req.body.company,
+            year: req.body.year
+        }
+        const result = await cars.insertOne(doc);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        res.redirect("/car");
+        } finally {
+            await client.close();
+        }
     });
-  } finally {
-    await client.close();
-  }
-});
-
-app.post("/car", async (req, res)=> {
-  try {
-    client.connect();
-    const database = client.db(dbName);
-    const cars = database.collection(collectionName);
-    const doc = {
-      name: req.body.name,
-      price: req.body.price,
-      company: req.body.company,
-      year: req.body.year
-    }
-    const result = await cars.insertOne(doc);
-    console.log(`A document was inserted with the _id: ${result.insertedId}`);
-    res.redirect("/car");
-  } finally {
-    await client.close();
-  }
-});
-
-
+  
 
 const server = http.createServer(app);
 server.listen(app.get('port'), ()=>{
   console.log(`서버 실행 중 >>> http://localhost:${app.get('port')}`);
-})
+});
---
+++ day06_mongo/src/server1.js
@@ -0,0 +1,73 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const { MongoClient } = require("mongodb");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+app.set('port', 3000);
+app.set("views", path.join(__dirname, "../views"));
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use("/", express.static(path.join(__dirname, "../public")) );
+
+const uri = "mongodb://localhost:27017";
+const client = new MongoClient(uri);
+const dbName = "vehicle";
+const collectionName = "car";
+
+app.get("/car", async (req, res) => {
+  try {
+    client.connect();
+    const database = client.db(dbName);
+    const cars = database.collection(collectionName);
+    const query = {};
+    const options = {
+      sort: { name: 1 },
+      //projection: { _id:true, name:1, company:1, price:1, year:1 }
+      projection: {}
+    };
+    const cursor = cars.find(query, options);
+    if ((await cars.countDocuments(query)) === 0) {
+      console.log("No documents found!");
+    }
+    const carList = [];
+    for await (const doc of cursor) {
+      carList.push(doc);
+      console.dir(doc);
+    }
+    req.app.render("CarList", {carList}, (err, html)=>{
+      res.end(html);
+    });
+  } finally {
+    await client.close();
+  }
+});
+
+app.post("/car", async (req, res)=> {
+  try {
+    client.connect();
+    const database = client.db(dbName);
+    const cars = database.collection(collectionName);
+    const doc = {
+      name: req.body.name,
+      price: req.body.price,
+      company: req.body.company,
+      year: req.body.year
+    }
+    const result = await cars.insertOne(doc);
+    console.log(`A document was inserted with the _id: ${result.insertedId}`);
+    res.redirect("/car");
+  } finally {
+    await client.close();
+  }
+});
+
+
+
+const server = http.createServer(app);
+server.listen(app.get('port'), ()=>{
+  console.log(`서버 실행 중 >>> http://localhost:${app.get('port')}`);
+})
