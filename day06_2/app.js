7bf031b5fef18fb5481c0f156e0571a2c80b06ae
rianolhh <lhh964513@gmail.com>
2024-08-27 16:48:27 +0900

    1

---
+++ day06_2/app.js
@@ -0,0 +1,452 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressSession = require("express-session");
+// 파일 업로드용 미들웨어
+var multer = require('multer');
+var fs = require('fs');
+
+// multer 미들웨어 사용: 미들웨어 사용 순서 
+// body-parser -> multer -> router 순으로 실행
+var storage = multer.diskStorage({
+    destination: function(req, file, callback) {
+        callback(null, 'uploads');
+    },
+    filename: function(req, file, callback) {
+        // 한글 파일명 깨짐 방지
+        const fileName = Buffer.from(file.originalname, 'latin1').toString('utf8');
+        // 파일명 중복을 방지하기 위한 처리
+        // Date.now() <-- 타임스템프
+        let index = fileName .lastIndexOf(".");
+        let newFileName = fileName .substring(0, index);
+        newFileName += Date.now();
+        newFileName += fileName .substring(index);
+        callback(null, newFileName);
+    }
+});
+// 파일 제한: 10개, 1G 이하
+var upload = multer({
+    storage: storage,
+    limits: {
+        files: 10,
+        fileSize: 1024 * 1024 * 1024
+    }
+});
+
+app.set('port', 3000);
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+// 외부
+app.use(express.static("public"));
+app.use('/uploads', express.static("uploads"));
+// POST 방식으로 파라미터 전달 받기 위한 설정
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+// 쿠키 사용 미들웨어 설정
+app.use(cookieParser());
+// 세션 미들웨어 등록
+app.use(expressSession({
+    secret: 'my key',
+    resave: true,
+    saveUninitialized: true
+}));
+
+// 임시 데이터 
+const memberList = [
+    {no:101, id:"user01", password:"1234", name:"홍길동", email:"hong@gmail.com"},
+    {no:102, id:"user02", password:"12345", name:"김길동", email:"kim@gmail.com"},
+    {no:103, id:"user03", password:"123", name:"박길동", email:"lee@gmail.com"},
+    {no:104, id:"user04", password:"123456", name:"이길동", email:"park@gmail.com"}
+];
+let noCnt = 105;
+
+// 쇼핑몰 상품 목록
+const carList = [
+    {
+        _id:111, 
+        name:'SM5', 
+        price:3000, 
+        year:1999, 
+        company:'SAMSUNG',
+        writedate: "",
+        photos: [
+            {
+                originalname: "르노삼성sm520.png", 
+                filename: "르노삼성sm520.png",
+                filesize: 371000,
+                mimetype: "img/png"
+            },{ 
+                originalname: "르노삼성sm5.png", 
+                filename: "르노삼성sm5.png",
+                filesize: 95900,
+                mimetype: "img/png"
+            }
+        ]
+    }
+];
+let carSeq=117;
+
+// 요청 라운팅 사용
+const router = express.Router();
+
+router.route("/home").get((req,res)=> {
+    req.app.render("home/Home", {}, (err, html)=>{
+        res.end(html);
+    });
+});
+
+router.route("/profile").get((req,res)=> {
+    req.app.render("profile/Profile", {}, (err, html)=>{
+        res.end(html);
+    });
+});
+
+//멤버 페이지, 로그인되어 있을때 화면을 보여줌
+router.route("/member").get((req,res)=> {
+    // 쿠키는 사용자쪽에 전달(res), 세션은 요청 들어올때 생성(req)
+    if(req.session.user !== undefined) {
+        const user = req.session.user;
+        req.app.render("member/Member", {user}, (err, html)=>{
+            res.end(html);
+        });
+    } else {
+        res.redirect("/login");
+    }
+});
+
+// ----- 로그인 페이지
+router.route("/login").get((req,res)=> {
+    req.app.render("member/Login", {}, (err, html)=>{
+        // 사용자(접속자)의 로컬에 쿠키가 저장 된다.
+        res.cookie('user', {
+            id:'TestUser',
+            name: '테스트 유저',
+            authorized: true
+        });
+        res.end(html);
+    });
+});
+
+router.route("/login").post((req,res)=> {
+    console.log(req.body.id, req.body.password);
+    const idx = memberList.findIndex(member=>member.id===req.body.id);
+    if(idx != -1) {
+        if(memberList[idx].password === req.body.password) {
+            console.log("로그인 성공!");
+            // 세션에 로그인 정보를 등록 후 멤버 페이지 이동
+            req.session.user = {
+                id: req.body.id,
+                name: memberList[idx].name,
+                email: memberList[idx].email, 
+                no: memberList[idx].no
+            }
+            res.redirect("/member");
+        } else {
+            console.log("로그인 실패! 패스워드가 맞지 않습니다.");
+            // 다시 로그인 페이지로 다시 이동
+            res.redirect("/login");
+        }
+    } else {
+        console.log("존재하지 않는 계정입니다.");
+        res.redirect("/login");
+    }
+});
+
+router.route("/logout").get((req, res)=>{
+    console.log("GET - /logout 호출 ...");
+    // 로그인 된 상태라면 로그아웃
+    if(!req.session.user) {
+        console.log("아직 로그인 전 상태입니다.");
+        res.redirect("/login");
+        return;
+    }
+    // 세션의 user 정보를 제거 해서 logout처리
+    req.session.destroy((err)=>{
+        if(err) throw err;
+        console.log("로그아웃 성공!");
+        res.redirect("/login");
+    });
+});
+
+router.route("/joinus").get((req,res)=> {
+    // 회원 가입 ejs 페이지 forward
+    req.app.render("member/Joinus", {}, (err, html)=>{
+        res.end(html);
+    });
+});
+
+router.route("/joinus").post((req,res)=> {
+    // 회원 가입 처리 후 목록으로 갱신
+    res.redirect("/member");
+});
+
+// ----- 로그인 기능 구현
+router.route("/gallery").get((req,res)=> {
+    req.app.render("gallery/Gallery", {}, (err, html)=>{
+        res.end(html);
+    });
+});
+
+// ---- 쇼핑몰 기능
+router.route("/shop").get((req,res)=> {
+    // forward ... 주소의 내용이 아닌 다른 파일의 내용 표시
+    req.app.render("shop/Shop", {carList}, (err, html)=>{
+        if(err) throw err;
+        res.end(html);
+    });
+});
+
+router.route("/shop/insert").get((req,res)=> {
+    req.app.render("shop/Insert", {}, (err, html)=>{
+        res.end(html);
+    });
+});
+
+router.route("/shop/insert").post(upload.array('photo', 1),(req,res)=> {
+    console.log("POST - /shop/insert");
+    // 구조분해 할당으로 body의 파라미터를 꺼낸다.
+    const {name, price, year, company} = req.body;
+    const newCar = {
+        _id:carSeq++, name, price, year, company,
+        writedate: Date.now(),
+        photos: []
+    };
+    newCar.photos = req.files;
+    carList.push(newCar);
+    ///res.send(carList);
+    res.redirect('/shop');
+});
+
+router.route("/shop/modify").get((req,res)=> {
+    const _id = parseInt(req.query._id);
+    console.log(_id)
+    const idx = carList.findIndex(car=>_id===car._id);
+    console.log(idx);
+    if(idx === -1) {
+        console.log("상품이 존재 하지 않습니다.")
+        res.redirect("/shop");
+        return;
+    }
+    req.app.render("shop/Modify", {car:carList[idx]}, (err, html)=>{
+        if(err) throw err;
+        res.end(html);
+    });
+});
+
+router.route("/shop/modify").post((req,res)=> {
+    console.log("POST - /shop/modify 호출");
+    console.dir(req.body);
+    res.redirect('/shop');
+});
+
+router.route("/shop/detail").get((req,res)=> {
+    // 쿼리로 전송된 데이터는 모두 문자열이다. 
+    // parseInt() 필수 "56" <-- numeric
+    const _id = parseInt(req.query._id);
+    //console.log(_id)
+    const idx = carList.findIndex(car=>_id===car._id);
+    //console.log(idx);
+    if(idx === -1) {
+        console.log("상품이 존재 하지 않습니다.")
+        res.redirect("/shop");
+        return;
+    }
+    req.app.render("shop/Detail", {car:carList[idx]}, (err, html)=>{
+        if(err) throw err;
+        res.end(html);
+    });
+});
+
+router.route("/shop/delete").get((req,res)=> {
+    req.app.render("shop/Delete", {}, (err, html)=>{
+        res.end(html);
+    });
+});
+
+router.route("/shop/cart").get((req,res)=> {
+    req.app.render("shop/Cart", {}, (err, html)=>{
+        res.end(html);
+    });
+});
+// --- 쇼핑몰 기능 끝
+
+// --- TodoList 기능 구현 시작
+const { MongoClient, ObjectId } = require("mongodb");
+
+const client = new MongoClient("mongodb://localhost:27017");
+const dbName = "comstudy";
+const collectionName = "todolist";
+
+// todolist 페이지 접속
+app.get("/todo/list", async (req,res)=>{
+    //res.render("todolist/TodoList", {todoList});
+    try {
+        await client.connect();
+        const database = client.db(dbName);
+        const todoCollection = database.collection(collectionName);
+        const QUERY = {};
+        const cursor = todoCollection.find(QUERY, {});
+        if ((await todoCollection.countDocuments(QUERY)) === 0) {
+          console.log("No documents found!");
+        }
+        const todoList = [];
+        for await (const doc of cursor) {
+            todoList.push(doc);
+        }
+        req.app.render("todolist/TodoList", {todoList}, (err, html)=>{
+            if(err) throw err;
+            res.end(html);
+        });
+      } finally {
+        await client.close();
+      }
+});
+
+//입력 페이지 접속
+app.get("/todo/input", (req,res)=>{
+    res.render("todolist/TodoInput", {});
+});
+
+// 상세보기 페이지
+app.get("/todo/detail", async (req,res)=>{
+    try {
+        await client.connect();
+        const database = client.db(dbName);
+        const collection = database.collection(collectionName);
+
+        const query = { _id: new ObjectId(req.query._id) };
+        const fetch = await collection.findOne(query);
+        console.log("Fetched document:", fetch);
+        res.render("todolist/TodoDetail", {todo: fetch});
+      } finally {
+        await client.close();
+      }
+});
+
+//수정 페이지 접속, db 데이터 송신
+app.get("/todo/modify", async (req,res)=>{
+    try {
+        await client.connect();
+        const database = client.db(dbName);
+        const collection = database.collection(collectionName);
+
+        const query = { _id: new ObjectId(req.query._id) };
+        const fetch = await collection.findOne(query);
+        console.log("Fetched document:", fetch);
+        res.render("todolist/TodoModify", {todo: fetch});
+      } finally {
+        await client.close();
+      }
+});
+
+// 저장 처리 - 몽고디비와 연동
+app.post("/todo/input", async (req,res)=>{
+    const doc = {
+        title: req.body.title,
+        done: (req.body.done=="true"?true:false)
+    }
+    console.dir(doc);
+    try {
+        await client.connect();
+        const database = client.db(dbName);
+        const todoCollection = database.collection(collectionName);
+        const result = await todoCollection.insertOne(doc);
+        console.log(`A document was inserted with the _id: ${result.insertedId}`);
+        res.redirect("/todo/list");
+      } finally {
+        await client.close();
+      }
+});
+
+
+app.get("/todo/detail", async (req, res) => {
+    try {
+        await client.connect();
+        const database = client.db(dbName);
+        const collection = database.collection(collectionName);
+
+        // 요청으로부터 전달된 _id 값을 이용하여 특정 항목을 조회
+        const query = { _id: new ObjectId(req.query._id) };
+        const todo = await collection.findOne(query);
+
+        if (todo) {
+            // 조회된 항목이 있다면 detail 페이지로 렌더링
+            res.render("todolist/TodoDetail", { todo: todo });  // ejs 파일에 todo 객체 전달
+        } else {
+            // 항목이 없으면 목록 페이지로 리다이렉트
+            console.log("존재하지 않는 todo 항목입니다.");
+            res.redirect("/todo/list");
+        }
+    } catch (err) {
+        console.error(err);
+    } finally {
+        await client.close();
+    }
+});
+
+//todo 수정 기능 구현
+app.post("/todo/modify", async (req,res)=>{
+    console.log(req.body._id);
+    try {
+        await client.connect();
+        const database = client.db(dbName);
+        const movies = database.collection(collectionName);
+        const filter = { _id: new ObjectId(req.body._id) };
+        const options = { upsert: true };
+        const updateDoc = {
+          $set: {
+            title: req.body.title,
+            done: (req.body.done=="true"?true:false)
+          }
+        };
+        const result = await movies.updateOne(filter, updateDoc, options);
+        console.log(`${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`,);
+        //res.redirect("/todo/list");
+    } finally {
+        await client.close();
+    }
+    res.redirect("/todo/list");
+});
+
+//todo 삭제기능 구현
+app.get("/todo/delete", async (req,res)=>{
+    try {
+        await client.connect();
+        const database = client.db(dbName);
+        const todos = database.collection(collectionName);
+        const query = { _id: new ObjectId(req.query._id) };
+        const result = await todos.deleteOne(query);
+        if (result.deletedCount === 1) {
+          console.log("Successfully deleted one document.");
+        } else {
+          console.log("No documents matched the query. Deleted 0 documents.");
+        }
+      } finally {
+        await client.close();
+      }
+    res.redirect("/todo/list");
+});
+// --- TodoList 기능 구현 끝
+
+// router 설정 맨 아래에 미들웨어 등록
+app.use('/', router);
+
+const expressErrorHandler = require('express-error-handler');
+
+//모든 라우터 처리 후 404 오류 페이지
+const errorHandler = expressErrorHandler({
+    static : {
+        '404':'./public/404.html'
+    }
+});
+app.use(expressErrorHandler.httpError(404) );
+app.use(errorHandler );
+
+// 서버 생성 및 실행
+const server = http.createServer(app);
+server.listen(app.get('port'), ()=>{
+    console.log(`Run on server >>> http://localhost:${app.get('port')}`);
+});
